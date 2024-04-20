4f65a9e3f7dd8cd9b3e3277da6069c6a5d4cf39b
CosminVulpe <cosmin98vulpe@gmail.com>
2024-04-20 17:43:21 +0400

    Add Api Gateway & Patience Management project structure

---
+++ patience-management/src/test/java/com/medicarepro/patiencemanagement/PatienceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicarepro.patiencemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatienceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
