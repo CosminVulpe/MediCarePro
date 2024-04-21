a5d3dc107033a2292cf94b5513f6d268c4b57db1
CosminVulpe <cosmin98vulpe@gmail.com>
2024-04-21 19:14:16 +0400

    Add Patience controller [PatienceManagement]

---
+++ patience-management/src/main/java/com/medicarepro/patiencemanagement/controller/PatienceController.java
@@ -0,0 +1,22 @@
+package com.medicarepro.patiencemanagement.controller;
+
+import com.medicarepro.patiencemanagement.controller.dto.PatienceDTO;
+import com.medicarepro.patiencemanagement.service.PatienceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/patience")
+public class PatienceController {
+    private final PatienceService patienceService;
+
+    @GetMapping
+    public List<PatienceDTO> getAllPatience() {
+        return patienceService.getAll();
+    }
+}
