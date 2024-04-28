4e3a216397d8343ce6927585e9a7ad9968f823e5
CosminVulpe <cosmin98vulpe@gmail.com>
2024-04-28 14:22:21 +0400

    Add functionality to get patience by id [PatienceManagementService]

--- patience-management/src/main/java/com/medicarepro/patiencemanagement/controller/PatienceController.java
+++ patience-management/src/main/java/com/medicarepro/patiencemanagement/controller/PatienceController.java
@@ -4,6 +4,7 @@ import com.medicarepro.patiencemanagement.controller.dto.PatienceDTO;
 import com.medicarepro.patiencemanagement.service.PatienceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +20,11 @@ public class PatienceController {
     public List<PatienceDTO> getAllPatience() {
         return patienceService.getAll();
     }
+
+    @GetMapping("/{patienceId}")
+    public PatienceDTO getAllPatience(@PathVariable Long patienceId) {
+        return patienceService.findPatienceById(patienceId);
+    }
+
+    
 }
--- patience-management/src/main/java/com/medicarepro/patiencemanagement/service/PatienceService.java
+++ patience-management/src/main/java/com/medicarepro/patiencemanagement/service/PatienceService.java
@@ -2,6 +2,7 @@ package com.medicarepro.patiencemanagement.service;
 
 import com.medicarepro.patiencemanagement.controller.dto.*;
 import com.medicarepro.patiencemanagement.service.entity.Patience;
+import com.medicarepro.patiencemanagement.service.exception.PatienceIdException;
 import com.medicarepro.patiencemanagement.service.mapping.ContractInformationMapping;
 import com.medicarepro.patiencemanagement.service.mapping.DemographicInformationMapping;
 import com.medicarepro.patiencemanagement.service.mapping.InsuranceInformationMapping;
@@ -19,14 +20,24 @@ public class PatienceService {
 
     public List<PatienceDTO> getAll() {
         List<Patience> patienceList = patienceRepository.findAll();
+        return patienceList.parallelStream().map(this::mapPatienceEntity).toList();
+    }
 
-        return patienceList.parallelStream().map(patience -> {
-            ContractInformationDTO contractInformationDTO = ContractInformationMapping.INSTANCE.convertToDto(patience.getContractInformation());
-            DemographicInformationDTO demographicInformationDTO = DemographicInformationMapping.INSTANCE.convertToDto(patience.getDemographicInformation());
-            InsuranceInformationDTO insuranceInformationDTO = InsuranceInformationMapping.INSTANCE.convertToDto(patience.getInsuranceInformation());
-            MedicalHistoryDTO medicalHistoryDTO = MedicalHistoryMapping.INSTANCE.convertToDto(patience.getMedicalHistory());
-            return new PatienceDTO(patience.getPatienceId(), demographicInformationDTO, contractInformationDTO, insuranceInformationDTO, medicalHistoryDTO);
-        }).toList();
+    public PatienceDTO findPatienceById(Long patienceId) {
+        return mapPatienceEntity(getPatienceOrThrow(patienceId));
+    }
+    
+
+    private Patience getPatienceOrThrow(Long patienceId) {
+        return patienceRepository.findById(patienceId).orElseThrow(() -> new PatienceIdException("Patience with ID cannot be found"));
+    }
+
+    private PatienceDTO mapPatienceEntity(Patience patience) {
+        ContractInformationDTO contractInformationDTO = ContractInformationMapping.INSTANCE.convertToDto(patience.getContractInformation());
+        DemographicInformationDTO demographicInformationDTO = DemographicInformationMapping.INSTANCE.convertToDto(patience.getDemographicInformation());
+        InsuranceInformationDTO insuranceInformationDTO = InsuranceInformationMapping.INSTANCE.convertToDto(patience.getInsuranceInformation());
+        MedicalHistoryDTO medicalHistoryDTO = MedicalHistoryMapping.INSTANCE.convertToDto(patience.getMedicalHistory());
+        return new PatienceDTO(patience.getPatienceId(), demographicInformationDTO, contractInformationDTO, insuranceInformationDTO, medicalHistoryDTO);
     }
 
 }
